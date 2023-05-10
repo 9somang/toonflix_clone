2bacbc5edea385a2283f1d5ad6cbabf4ab82d6e1
somang Ku <ksmang09@naver.com>
2023-05-10 17:45:54 +0900

    The end.
    end

---
+++ toonflix/lib/widgets/episose_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../models/webtoon_episode_model.dart';
+
+class Episode extends StatelessWidget {
+  const Episode({
+    super.key,
+    required this.episode,
+    required this.webtoonId,
+  });
+  final String webtoonId;
+  final WebtoonEpisodeModel episode;
+
+  onButtonTap() async {
+    final url = Uri.parse(
+        "https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episode.id}");
+    await launchUrl(url);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Colors.green),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: TextStyle(
+                  color: Colors.green.shade400,
+                  fontSize: 16,
+                ),
+              ),
+              Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.green.shade400,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
